055f7b8fd2f053bbbdf807f6552fd7598efa4664
Azazel_261 <george.blagoveshchenskiy@gmail.com>
2026-08-30 08:47:14 +0200

    Working command skeleton + basic autocompletes (still WIP). I'm hesitant to merge this into main

--- commands.py
+++ commands.py
@@ -2,22 +2,23 @@ from typing import Callable, Coroutine, Any
 import hikari
 import data
 
-async def help_command(body: str, event: hikari.GuildMessageCreateEvent, bot: hikari.GatewayBot):
+async def help_command(interaction: hikari.CommandInteraction, bot: hikari.RESTBot):
+    await interaction.create_initial_response(hikari.ResponseType.DEFERRED_MESSAGE_CREATE, flags=hikari.MessageFlag.EPHEMERAL)
     out = "Unknown help section"
-    match body:
-        case "":
+    match interaction.options[0].name:
+        case "commands":
             out = data.help_message
-        case "attr":
+        case "attributes":
             out = data.attribute_help_message
-    await bot.rest.create_message(event.message.channel_id, out)
+    await interaction.edit_initial_response(out)
 
-async def create_command(body: str, event: hikari.GuildMessageCreateEvent, bot: hikari.GatewayBot):
+async def create_command(interaction: hikari.CommandInteraction, bot: hikari.RESTBot):
     pass
 
-async def join_command(body: str, event: hikari.GuildMessageCreateEvent, bot: hikari.GatewayBot):
+async def join_command(interaction: hikari.CommandInteraction, bot: hikari.RESTBot):
     pass
 
-commands: dict[str, Callable[[str, hikari.GuildMessageCreateEvent, hikari.GatewayBot], Coroutine[Any, Any, None]]] = {
+commands: dict[str, Callable[[hikari.CommandInteraction, hikari.RESTBot], Coroutine[Any, Any, None]]] = {
     "help" : help_command,
     "create": create_command,
     "join" : join_command
--- main.py
+++ main.py
@@ -1,41 +1,79 @@
 import os
 import hikari
 import commands
-import data
 
 token = os.getenv("DISCORD_TOKEN")
+public_key = os.getenv("PUBLIC_KEY")
 
-if not token:
+if not token or not public_key:
     exit(-1)
 
-bot = hikari.GatewayBot(token, intents=hikari.Intents.MESSAGE_CONTENT | hikari.Intents.GUILD_MESSAGES)
+async def handle_command(interaction: hikari.CommandInteraction):
+    command_path = [interaction.command_name]
 
-# Guaranteed to receive a command that starts with "ping! " as the prefix
-async def handle_command(event: hikari.GuildMessageCreateEvent):
-    if not event.message.content:
-        return
-    command_text = event.message.content.removeprefix("ping! ")
-    w = command_text.find(" ")
-    command_name = command_text[:w] if w >= 0 else command_text
-    command = commands.get(command_name)
+    subcommands = [_ for _ in interaction.options if _.type == hikari.OptionType.SUB_COMMAND or _.type == hikari.OptionType.SUB_COMMAND_GROUP]
+
+    for sub in subcommands:
+        command_path.append(sub.name)
+
+    command = commands.get(" ".join(command_path)) or commands.get(interaction.command_name)
+
     if command:
-        await command(command_text.removeprefix(command_name).removeprefix(" "), event, bot)
+        await command(interaction, bot)
+    else:
+        await interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Internal error", flags=hikari.MessageFlag.EPHEMERAL)
 
-@bot.listen()
-async def on_join(event: hikari.GuildJoinEvent):
-    if event.guild.system_channel_id:
-        await bot.rest.create_message(event.guild.system_channel_id, data.join_message)
+async def handle_autocomplete(interaction: hikari.AutocompleteInteraction):
+    return interaction.build_response(
+        [
+            hikari.impl.AutocompleteChoiceBuilder(name="foo", value="a"),
+            hikari.impl.AutocompleteChoiceBuilder(name="bar", value="b"),
+            hikari.impl.AutocompleteChoiceBuilder(name="baz", value="c"),
+        ]
+    )
 
-@bot.listen()
-async def on_message(event: hikari.GuildMessageCreateEvent):
-    if event.author.is_bot:
-        return
+async def create_commands(_bot: hikari.RESTBot):
+    application = await _bot.rest.fetch_application()
 
-    if not event.message.content:
-        return
+    help_command = _bot.rest.slash_command_builder("help", "Get help with using SmartPing!")
+    help_command.add_option(hikari.CommandOption(type=hikari.OptionType.SUB_COMMAND, name="commands", description="List bot commands"))
+    help_command.add_option(hikari.CommandOption(type=hikari.OptionType.SUB_COMMAND, name="attributes", description="List all possible group attributes"))
 
-    if event.message.content.startswith("ping! "):
-        await handle_command(event)
+    group_command = _bot.rest.slash_command_builder("group", "Manage groups").set_context_types([hikari.ApplicationContextType.GUILD])
+    group_command.add_option(hikari.CommandOption(type=hikari.OptionType.SUB_COMMAND, name="create", description="Create a new call group",
+                                                   options=[
+                                                       hikari.CommandOption(type=hikari.OptionType.STRING, name="name",
+                                                                            description="Group name", max_length=32,
+                                                                            is_required=True),
+                                                       hikari.CommandOption(type=hikari.OptionType.BOOLEAN,
+                                                                            name="private",
+                                                                            description="Make the group invite-only",
+                                                                            is_required=False)
+                                                   ]))
+    group_command.add_option(hikari.CommandOption(type=hikari.OptionType.SUB_COMMAND, name="join", description="Join an existing call group",
+                                                  options=[
+                                                      hikari.CommandOption(type=hikari.OptionType.STRING, name="name",
+                                                                           description="Group name", max_length=32, is_required=True, autocomplete=True)
+                                                  ]))
+    group_command.add_option(hikari.CommandOption(type=hikari.OptionType.SUB_COMMAND, name="leave", description="Leave a call group",
+                                                  options=[
+                                                      hikari.CommandOption(type=hikari.OptionType.STRING, name="name",
+                                                                           description="Group name", max_length=32, is_required=True, autocomplete=True)
+                                                  ]))
+    group_command.add_option(hikari.CommandOption(type=hikari.OptionType.SUB_COMMAND, name="leaveall", description="Leave all call groups in the server"))
 
-if __name__ == "__main__":
-    bot.run()
+    await _bot.rest.set_application_commands(
+        application=application.id,
+        commands=[
+            help_command,
+            group_command
+        ],
+    )
+
+bot = hikari.RESTBot(token=token, public_key=public_key)
+
+bot.add_startup_callback(create_commands)
+bot.set_listener(hikari.CommandInteraction, handle_command)
+bot.set_listener(hikari.AutocompleteInteraction, handle_autocomplete)
+
+bot.run()
